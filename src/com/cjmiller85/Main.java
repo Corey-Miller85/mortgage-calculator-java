7ec10b4061b2c155c0e3849954fc64e1f85d6011
Corey Miller <coreyMiller353@gmail.com>
2020-06-29 14:29:52 -0400

    add validation to input

--- src/com/cjmiller85/Main.java
+++ src/com/cjmiller85/Main.java
@@ -9,16 +9,40 @@ public class Main {
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         NumberFormat percent = NumberFormat.getPercentInstance();
         Scanner scanner = new Scanner(System.in);
+        int principle = 0;
+        float interestRate = 0F;
+        int period = 0;
 
-        System.out.print("Principle: ");
-        int principle = scanner.nextInt();
+        while (principle == 0) {
+            System.out.print("Principle ($1K - $1M): ");
+            int input = scanner.nextInt();
+            if (input < 1000) {
+                System.out.println("Please enter a value between or equal to 1,000 and 1,000,000");
+            } else {
+                principle = input;
+            }
+        }
+        while (interestRate == 0F) {
+            System.out.print("Annual Interest Rate (0-30): ");
+            float input = scanner.nextFloat();
+            if (input < 0F || input > 30F) {
+                System.out.println("Please enter a value between or equal to 0 and 30");
+            } else {
+                interestRate = input;
+            }
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float interestRate = scanner.nextFloat() ;
+        while (period == 0) {
+            System.out.print("Period (years): ");
+            int input = scanner.nextInt();
+            if (input < 0 || input > 30) {
+                System.out.println("Please enter a value between or equal to 0 and 30");
+            } else {
+                period = input;
+            }
+        }
+
         float monthlyInterestRate = interestRate / 100 / 12;
-
-        System.out.print("Period (years): ");
-        int period = scanner.nextInt();
         int payments = period * 12;
 
         String mortgage = currency.format(principle
