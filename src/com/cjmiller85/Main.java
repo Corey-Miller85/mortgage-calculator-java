fd9b725b565aea9511001a8073bc570f87195fdb
Corey Miller <coreyMiller353@gmail.com>
2020-06-29 15:31:05 -0400

    refactor code

--- src/com/cjmiller85/Main.java
+++ src/com/cjmiller85/Main.java
@@ -6,46 +6,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int principle;
-        float annualInterest;
-        byte years;
-
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        NumberFormat percent = NumberFormat.getPercentInstance();
-        Scanner scanner = new Scanner(System.in);
-
-
-        while (true) {
-            System.out.print("Principle ($1K - $1M): ");
-            int input = scanner.nextInt();
-            if (input >= 1000 && input <= 1_000_000) {
-                principle = input;
-                break;
-            } else {
-                System.out.println("Please enter a value between or equal to 1,000 and 1,000,000");
-            }
-        }
-        while (true) {
-            System.out.print("Annual Interest Rate (0-30): ");
-            annualInterest = scanner.nextFloat();
-            if (annualInterest > 0F && annualInterest <= 30F) {
-                break;
-            }
-            System.out.println("Please enter a value between or equal to 0 and 30");
-
-        }
-
-        while (true) {
-            System.out.print("Period (years): ");
-            years = scanner.nextByte();
-            if (years > 0 && years <= 30)
-                break;
-            System.out.println("Please enter a value between or equal to 0 and 30");
-        }
+        int principle = (int) readNumber("Principle: ", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate (0-30): ", 1, 30);
+        byte years = (byte) readNumber("Period (Years): ", 1, 30);
 
         double mortgage = calculateMortgage(principle, annualInterest, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("-----------------\n" + "Mortgage is: " + mortgageFormatted);
+    }
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Please enter a value between or equal to " + min + " and " + max );
+        }
+        return value;
     }
 
     public static double calculateMortgage(int principle, float annualInterest, byte years) {
